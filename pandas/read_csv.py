7ca3ac54c46595dc257a69b5a211ce72fdef839c
Sharvil Nikam <sharvilgnikam15@gmail.com>
2026-07-10 11:05:08 +0530

    Learn to fill missing values in pandas

--- pandas/read_csv.py
+++ pandas/read_csv.py
@@ -5,7 +5,15 @@ print(df)
 print(type(df))
 print(df.dtypes)
 
+#find missing values
 print(df.isna())
 print(df[df["Rating"].isna()])
 print(df[df["Year"].isna()])
 print(df.isna().sum())
+
+#cleaning missing data
+filled_df = df.fillna(0)
+print(filled_df)
+
+df["Rating"] = df["Rating"].fillna(0)
+print(df)
